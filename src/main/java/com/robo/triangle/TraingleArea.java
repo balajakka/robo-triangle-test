213bf3706dd2cd6be11aa08e4e0b86501dd518d5
Bala Jakka <Bala.Jakka@ig.com>
2016-01-14 18:20:32 +0000

    added Junit tests

--- src/main/java/com/robo/triangle/TraingleArea.java
+++ src/main/java/com/robo/triangle/TraingleArea.java
@@ -4,5 +4,25 @@
  */
 
 package com.robo.triangle;
+
+import java.text.DecimalFormat;
+
 public class TraingleArea {
+
+   double height;
+   double base;
+   double areaOfTriangle;
+
+   public double calcAreaOfRightTriangle(double height, double base) throws Exception {
+
+      if(height>9999999.99||base>9999999.99||height<0||base<0){
+         throw new Exception("you have entered more than 8 digits");
+      }
+      else {
+
+         double r = (base * height) / 2;
+         DecimalFormat twoDForm = new DecimalFormat("0.00");
+         return r;
+      }
+   }
 }
--- src/test/java/com/robo/triangleTest/TriangleArealTest.java
+++ src/test/java/com/robo/triangleTest/TriangleArealTest.java
@@ -2,5 +2,62 @@
  * Created by jakkab on 13/01/2016.
  */
 package com.robo.triangleTest;
+
+import com.robo.triangle.TraingleArea;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TriangleArealTest {
+
+   @Test
+   public void areaCalculationForDouble() throws Exception {
+
+      TraingleArea tArea = new TraingleArea();
+
+      Assert.assertTrue( "area of triangle with height 11.11 and base 12.12 is not correct",tArea.calcAreaOfRightTriangle(11.11, 12.12)==67.3266);
+
+   }
+
+   @Test
+   public void areaCalculationForZero() throws Exception {
+
+      TraingleArea tArea = new TraingleArea();
+
+      Assert.assertTrue( "area of triangle with height 0 and base 12.12 is not correct",tArea.calcAreaOfRightTriangle(0, 12.12)==0);
+
+   }
+
+   @Test
+   public void areaCalculationForInt() throws Exception {
+
+      TraingleArea tArea = new TraingleArea();
+
+      Assert.assertTrue("area of triangle with height 11 and base 12 is not correct", tArea.calcAreaOfRightTriangle(11, 12) == 66.0);
+
+   }
+
+   @Test
+   public void testareaCalculationThrowsExceptionForGreaterThan8Digits() {
+      boolean thrown = false;
+      TraingleArea tArea = new TraingleArea();
+      try {
+         tArea.calcAreaOfRightTriangle(111111111, 12);
+      } catch (Exception e) {
+         thrown = true;
+      }
+
+      Assert.assertTrue("Not throwing exception for more than 8 digits",thrown);
+   }
+   @Test
+   public void testareaCalculationThrowsExceptionForNetgativeValues() {
+      boolean thrown = false;
+      TraingleArea tArea = new TraingleArea();
+      try {
+         tArea.calcAreaOfRightTriangle(-1, 12);
+      } catch (Exception e) {
+         thrown = true;
+      }
+
+      Assert.assertTrue("Not throwing exception for negative values",thrown);
+   }
 }
